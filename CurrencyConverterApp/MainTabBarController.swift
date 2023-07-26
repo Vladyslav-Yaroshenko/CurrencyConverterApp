12d813a9f02173776bae3ca1c4c426c1e00e970b
Vladyslav-Yaroshenko <vlad.yaroshenko7@gmail.com>
2023-07-26 14:17:43 +0300

    Removing a storyboard from the project. Launching the application from sceneDelegate. MainTabBarController created

--- CurrencyConverterApp/MainTabBarController.swift
+++ CurrencyConverterApp/MainTabBarController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainTabBarController.swift
 //  CurrencyConverterApp
 //
 //  Created by Vlad on 26.07.2023.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
     }
-
-
 }
-
