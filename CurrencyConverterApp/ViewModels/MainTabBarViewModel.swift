41ca299254576b12b486afa41b06010cfea75eb5
Vladyslav-Yaroshenko <vlad.yaroshenko7@gmail.com>
2023-07-27 15:50:35 +0300

    Created SettingVC

--- CurrencyConverterApp/ViewModels/MainTabBarViewModel.swift
+++ CurrencyConverterApp/ViewModels/MainTabBarViewModel.swift
@@ -10,8 +10,8 @@ import UIKit
 class MainTabBarViewModel: MainTabBarViewModelProtocol {
     var tabBarItems: [MainTabBarItemModel] = [
         MainTabBarItemModel(title: "Currency List", imageName: "currency-dollar"),
-        MainTabBarItemModel(title: "Wallet", imageName: "wallet")
+        MainTabBarItemModel(title: "Wallet", imageName: "wallet"),
 //        MainTabBarItemModel(title: "Bids", imageName: "credit-card"),
-//        MainTabBarItemModel(title: "Settings", imageName: "settings")
+        MainTabBarItemModel(title: "Settings", imageName: "settings")
     ]
 }
---
+++ CurrencyConverterApp/Views/SettingsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsViewController.swift
+//  CurrencyConverterApp
+//
+//  Created by Vlad on 27.07.2023.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+    }
+}
